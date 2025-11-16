cb0675495c18f92a6b968be5b072d6b1633770e7
Met-s <den_va@mail.ru>
2025-11-16 09:56:21 +0300

    modified

--- Java_Klass_2_Automatic_Testing_Gradle/build.gradle.kts
+++ Java_Klass_2_Automatic_Testing_Gradle/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation ("com.fasterxml.jackson.core:jackson-annotations:2.12.4")
     implementation ("com.fasterxml.jackson.core:jackson-core:2.12.4")
     implementation("com.fasterxml.jackson.dataformat:jackson-dataformat-yaml:2.19.1")
+
     testImplementation("org.assertj:assertj-core:4.0.0-M1")
     testImplementation ("org.junit.jupiter:junit-jupiter-params:5.10.0")
     implementation("net.datafaker:datafaker:2.4.2")
