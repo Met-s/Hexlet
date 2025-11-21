75011397792e58f19bb91e19c9e194ea7a750874
Met-s <den_va@mail.ru>
2025-11-21 17:25:06 +0300

    Init: Java_ООП

---
+++ Java_OOP/build.gradle.kts
@@ -0,0 +1,34 @@
+plugins {
+    id("java")
+    id("idea")
+    application
+}
+
+group = "hexlet.code"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+}
+
+application {
+    mainClass.set("hexlet.code.App")
+}
+
+dependencies {
+    testImplementation(platform("org.junit:junit-bom:5.10.0"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
+
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.junit.platform:junit-platform-console:1.13.1")
+    implementation("org.apache.commons:commons-lang3:3.18.0")
+    compileOnly ("org.projectlombok:lombok:1.18.36")
+    annotationProcessor ("org.projectlombok:lombok:1.18.36")
+}
+
+tasks.test {
+    useJUnitPlatform()
+    testLogging {
+        events ("passed", "skipped", "failed")
+    }
+}
