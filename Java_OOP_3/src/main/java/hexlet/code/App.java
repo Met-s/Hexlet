da76bd5bc1c656d5be05e963d413a5c89623e215
Met-s <feronts@mail.ru>
2025-12-09 11:45:00 +0300

    renamed Java_OOP in Java_OOP_3

---
+++ Java_OOP_3/src/main/java/hexlet/code/App.java
@@ -0,0 +1,38 @@
+package hexlet.code;
+
+import hexlet.code.exercise1.Point;
+import hexlet.code.exercise1.Segment;
+
+import java.util.Arrays;
+
+public class App {
+    public static void main(String[] args) {
+//        System.out.println("Hello World!");
+/**
+ * ###_____ Страница модуля ____###
+ * №_1
+ */
+//        var person = new Person();
+//        person.setName("Agata");
+//        person.setAge(10);
+//        System.out.println(person);
+/**
+*  ###_____ Задание ____###
+ * №_1
+*/
+
+//        Point point1 = new Point(4, 3);
+//        Point point2 = new Point(6, 1);
+//
+//        Segment segment = new Segment(point1, point2);
+//        Point midPoint = segment.getMidPoint();
+//        int x = midPoint.getX();  // 5
+//        int y = midPoint.getY();  // 2
+//
+//        System.out.println("x = " + x + "\ny = " + y);
+
+
+
+
+    }
+}
